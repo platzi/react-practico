da0897411c68f9293530944351d33c442b0cd75f
Oscar Barajas <oscar@gndx.dev>
2021-08-23 16:32:16 -0500

    :sparkles: [ADD] Icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import '../styles/Header.scss';
+import '@styles/Header.scss';
+
+import menu from '@icons/icon_menu.svg';
+import logo from '@logos/logo_yard_sale.svg';
+import shoppingCart from '@icons/icon_shopping_cart.svg';
 
 const Header = () => {
 	return (
 		<nav>
-			<img src="./icons/icon_menu.svg" alt="menu" className="menu" />
+			<img src={menu} alt="menu" className="menu" />
 			<div className="navbar-left">
-				<img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
+				<img src={logo} alt="logo" className="nav-logo" />
 				<ul>
 					<li>
 						<a href="/">All</a>
@@ -32,7 +36,7 @@ const Header = () => {
 				<ul>
 					<li className="navbar-email">platzi@example.com</li>
 					<li className="navbar-shopping-cart">
-						<img src="./icons/icon_shopping_cart.svg" alt="shopping cart" />
+						<img src={shoppingCart} alt="shopping cart" />
 						<div>2</div>
 					</li>
 				</ul>
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,13 @@ module.exports = {
 	mode: 'development',
 	resolve: {
 		extensions: ['.js', '.jsx'],
+		alias: {
+			'@components': path.resolve(__dirname, 'src/components/'),
+			'@containers': path.resolve(__dirname, 'src/containers/'),
+			'@styles': path.resolve(__dirname, 'src/styles/'),
+			'@icons': path.resolve(__dirname, 'src/assets/icons/'),
+			'@logos': path.resolve(__dirname, 'src/assets/logos/'),
+		}
 	},
 	module: {
 		rules: [
@@ -37,6 +44,10 @@ module.exports = {
 					"css-loader",
 					"sass-loader",
 				],
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				type: 'asset'
 			}
 		]
 	},
