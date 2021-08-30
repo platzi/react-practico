d3ba9705813061e19c0dc1d0226a01108eb34394
Oscar Barajas <oscar@gndx.dev>
2021-08-30 17:05:45 -0500

    :sparkles: [ADD] useEffect

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductItem from '@components/ProductItem';
 import '@styles/ProductList.scss';
+import axios from 'axios';
+
+const API = 'http://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
+	const [products, setProducts] = useState([]);
+
+	useEffect(async () => {
+		const response = await axios(API);
+		setProducts(response.data);
+	}, [])
+
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map(product => (
+					<ProductItem />
+				))}
 			</div>
 		</section>
 	);
