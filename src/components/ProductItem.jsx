276b635f3d30ede6b5433414161ba5afe098f9b3
Oscar Barajas <oscar@gndx.dev>
2021-08-30 17:28:40 -0500

    :sparkles: [ADD] Custom hooks

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import '@styles/ProductItem.scss';
 import addToCart from '@icons/bt_add_to_cart.svg';
 
-const ProductItem = () => {
+const ProductItem = ({ product }) => {
 	const [cart, setCart] = useState([]);
 
 	const handleClick = () => {
@@ -11,11 +11,11 @@ const ProductItem = () => {
 
 	return (
 		<div className="ProductItem">
-			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+			<img src={product.images[0]} alt={product.title} />
 			<div className="product-info">
 				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
+					<p>${product.price}</p>
+					<p>{product.title}</p>
 				</div>
 				<figure onClick={handleClick} >
 					<img src={addToCart} alt="" />
--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,23 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ProductItem from '@components/ProductItem';
+import useGetProducts from '../hooks/useGetProducts';
 import '@styles/ProductList.scss';
-import axios from 'axios';
 
 const API = 'http://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
-	const [products, setProducts] = useState([]);
-
-	useEffect(async () => {
-		const response = await axios(API);
-		setProducts(response.data);
-	}, [])
+	const products = useGetProducts(API);
 
 	return (
 		<section className="main-container">
 			<div className="ProductList">
 				{products.map(product => (
-					<ProductItem />
+					<ProductItem product={product} key={product.id} />
 				))}
 			</div>
 		</section>
