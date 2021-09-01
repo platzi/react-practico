8cde8e621914266f83098f161b9bf99c0292e788
Oscar Barajas <oscar@gndx.dev>
2021-09-01 18:19:05 -0500

    [FIX] https

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -3,7 +3,7 @@ import ProductItem from '@components/ProductItem';
 import useGetProducts from '../hooks/useGetProducts';
 import '@styles/ProductList.scss';
 
-const API = 'http://api.escuelajs.co/api/v1/products';
+const API = 'https://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
 	const products = useGetProducts(API);
