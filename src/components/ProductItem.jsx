a9e8257c0194f0d5f7961cd9c98cfd2ab5761fb3
Antonio Cruz Hernandez <andejecruher@gmail.com>
2022-08-28 12:58:13 -0500

    Hooks

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
-import '../styles/ProductItem.scss';
-
+import React , { useState } from 'react';
+import '@styles/ProductItem.scss';
+import addToCart from '@icons/bt_add_to_cart.svg';
 const ProductItem = () => {
+	const [cart, setCart] = useState('');
+	const handleClick = () => {
+		setCart('Hola mundo');
+	};
 	return (
 		<div className="ProductItem">
 			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
@@ -10,12 +14,13 @@ const ProductItem = () => {
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
-					<img src="./icons/bt_add_to_cart.svg" alt="" />
+				<figure onClick={handleClick}>
+					<img src={addToCart} alt="add to cart" />
 				</figure>
+				{cart}
 			</div>
 		</div>
 	);
-}
+};
 
 export default ProductItem;
